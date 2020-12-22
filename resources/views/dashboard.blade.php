83104eaa37c50cf561f74379efed005456663dcf
xavir <xaviranik.me@gmail.com>
2020-12-22 21:51:17 +0600

    add: dashboard courses section

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,20 +1,75 @@
 <x-app-layout>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white border-l-8 border-blue-500 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-5 bg-white border-b border-gray-200">
-                    <div class="text-lg font-semibold text-gray-900">
+            <div class="bg-white border-l-8 border-blue-500 overflow-hidden sm:rounded-lg">
+                <div class="p-4 border-b border-gray-200">
+                    <div class="flex items-center justify-between">
                         <div class="flex items-center">
                             <svg class="w-5 text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24"
                                 xmlns="http://www.w3.org/2000/svg">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1"
                                     d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253">
                                 </path>
                             </svg>
-                            <div class="ml-2">
+                            <div class="text-md font-base text-gray-500 ml-2">
                                 Let's buy some courses. Shall we?
                             </div>
                         </div>
+                        <svg class="w-5 text-red-400" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                        </svg>
+                    </div>
+                </div>
+            </div>
+            <div class="mt-6 grid grid-cols-2 gap-6">
+                <div class="col-span-1 bg-white p-6 rounded-lg shadow">
+                    <div class="flex items-center font-bold text-gray-900 text-2xl"><svg class="w-6" fill="none"
+                            stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5"
+                                d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"></path>
+                        </svg>
+                        <div class="ml-2">Learn Python Programming Masterclass</div>
+                    </div>
+                    <div class="mt-2 text-sm text-gray-500">This Python For Beginners Course Teaches You The Python
+                        Language Fast. Includes Python Online Training With Python 3</div>
+                    <div class="mt-4 text-gray-500 text-sm">Created by <span class="underline text-gray-700">John
+                            Cena</span></div>
+                    <div class="mt-6 text-gray-900 font-bold underline flex items-center"><svg class="w-6 h-6"
+                            fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z">
+                            </path>
+                        </svg>
+                        <div class="ml-2">Preview this course</div>
+                    </div>
+                    <div class="mt-6 text-2xl font-bold text-gray-900">$40.00</div>
+                    <div class="mt-6">
+                        <a class="bg-gray-900 text-white text-md px-5 py-2 rounded-md hover:bg-gray-800" href="#">Buy Course</a>
+                    </div>
+                </div>
+                <div class="col-span-1 bg-white p-6 rounded-lg shadow">
+                    <div class="flex items-center font-bold text-gray-900 text-2xl"><svg class="w-6" fill="none"
+                            stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5"
+                                d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"></path>
+                        </svg>
+                        <div class="ml-2">Java Programming for Software Developers</div>
+                    </div>
+                    <div class="mt-2 text-sm text-gray-500">Learn Java In This Course And Become a Computer Programmer. Obtain valuable Core Java Skills And Java Certification</div>
+                    <div class="mt-4 text-gray-500 text-sm">Created by <span class="underline text-gray-700">Tyrion Lannister</span></div>
+                    <div class="mt-6 text-gray-900 font-bold underline flex items-center"><svg class="w-6 h-6"
+                            fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z">
+                            </path>
+                        </svg>
+                        <div class="ml-2">Preview this course</div>
+                    </div>
+                    <div class="mt-6 text-2xl font-bold text-gray-900">$120.99</div>
+                    <div class="mt-6">
+                        <a class="bg-gray-900 text-white text-md px-5 py-2 rounded-md hover:bg-gray-800" href="#">Buy Course</a>
                     </div>
                 </div>
             </div>
