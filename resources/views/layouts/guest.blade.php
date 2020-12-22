de83b1703f1b881663749e27f97b7a4a80d1684f
xavir <xaviranik.me@gmail.com>
2020-12-22 19:57:29 +0600

    add: welcome view, guest layout

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -17,7 +17,7 @@
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body>
-        <div class="font-sans text-gray-900 antialiased">
+        <div class="font-sans bg-gray-200 text-gray-900 antialiased">
             {{ $slot }}
         </div>
     </body>
